7a1ce46231eff947f08dd8074774c1dca0b317f7
Dev Sharma <103065378+voyagerdev26@users.noreply.github.com>
2026-03-18 10:13:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+
+let bolds=document.querySelectorAll("strong");
 function highlight() {
     //Write your code here
+	for(let b of bolds){
+		b.style.color="rgb(0, 128, 0)";
+	}
 
 
 }
@@ -7,6 +12,9 @@ function highlight() {
 
 function return_normal() {
     //Write your code here
+	for(let b of bolds){
+		b.style.color="rgb(0, 0, 0)";
+	}
 
     
 }
